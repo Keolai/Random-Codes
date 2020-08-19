213489bda2432e30b62b07b9572cda5a6d0d7c02
Keolai <69029671+Keolai@users.noreply.github.com>
2020-08-18 19:59:32 -0500

    Update Factors.cpp

--- Factors.cpp
+++ Factors.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <string> 
-
-//this program finds all of the possible factors of a given number
+#include <chrono> 
 
 using namespace std;
+using namespace std::chrono; 
 
 int main(){ 
+
+int d = 0;
+int y = 0; 
+
+while (d<1){
+
+int f = 0; 
  
 std::vector<int> factors;   
 factors.push_back(1); 
@@ -17,9 +24,12 @@ std::cin >> numb;
 
 int begnumb; 
 int i = 2;
-int math;  
+int math; 
+int posnumb; 
 
 begnumb = numb; 
+
+auto start = high_resolution_clock::now(); 
 
 while (i != begnumb) {
     math= numb % i; 
@@ -34,10 +44,26 @@ while (i != begnumb) {
     
 }
 
+
 factors.push_back(begnumb); 
+
+auto stop = high_resolution_clock::now(); 
+auto duration = duration_cast<microseconds>(stop - start); 
 
 std::cout << "The factors of " << begnumb << " are:\n"; 
 for (std::vector<int>::const_iterator x = factors.begin(); x != factors.end(); ++x)
     std::cout << *x << "\n";
+std::cout << "Time taken to calculate: " << duration.count() << " microseconds\n";
+
+std::cout << "Would you like to factor another number?\n""(1-yes and 2-no)\n";
+;std::cin >> f;
+
+    if (f==2) {
+		d++;
+	} else {
+    	y++;
+	}
+
+}
 
 } 
